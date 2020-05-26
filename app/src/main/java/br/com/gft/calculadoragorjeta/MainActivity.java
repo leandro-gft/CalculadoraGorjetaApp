98eebd1185e0e38bda63b2f8de19f131df9bdc38
leandro-gft <lpsacchi@gmail.com>
2020-05-26 12:46:08 -0300

    Adicionadas as views do projeto

--- app/src/main/java/br/com/gft/calculadoragorjeta/MainActivity.java
+++ app/src/main/java/br/com/gft/calculadoragorjeta/MainActivity.java
@@ -3,12 +3,30 @@ package br.com.gft.calculadoragorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextInputEditText etValor;
+    private SeekBar seekBar;
+    private TextView tvSeekBar;
+    private TextView tvGorjeta;
+    private TextView tvTotal;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etValor = findViewById(R.id.etValor);
+        seekBar = findViewById(R.id.seekBar);
+        tvSeekBar = findViewById(R.id.tvSeekBar);
+        tvGorjeta = findViewById(R.id.tvGorjeta);
+        tvTotal = findViewById(R.id.tvTotal);
     }
-}
+    }
