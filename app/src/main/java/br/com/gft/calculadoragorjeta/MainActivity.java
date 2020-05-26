b5af509e0b67744dfaa80a07db8b918367b6728f
leandro-gft <lpsacchi@gmail.com>
2020-05-26 13:10:07 -0300

    Adicionada classe para realizar o calculo da gorjeta e do total. Fim do projeto

--- app/src/main/java/br/com/gft/calculadoragorjeta/MainActivity.java
+++ app/src/main/java/br/com/gft/calculadoragorjeta/MainActivity.java
@@ -28,5 +28,41 @@ public class MainActivity extends AppCompatActivity {
         tvSeekBar = findViewById(R.id.tvSeekBar);
         tvGorjeta = findViewById(R.id.tvGorjeta);
         tvTotal = findViewById(R.id.tvTotal);
+
+
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                int taxaGorjeta = seekBar.getProgress();
+                tvSeekBar.setText(taxaGorjeta+"%");
+
+                try {
+                    calcular(taxaGorjeta);
+                }catch (NumberFormatException e){
+                    Toast.makeText(getApplicationContext(),"Entre com um valor.", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+
     }
+
+    private void calcular(int taxaGorjeta) {
+        double valor = Double.parseDouble(etValor.getText().toString());
+        double gorjeta = valor * taxaGorjeta/100;
+        double total = gorjeta + valor;
+        tvGorjeta.setText("R$"+gorjeta);
+        tvTotal.setText("R$"+total);
     }
+
+
+}
